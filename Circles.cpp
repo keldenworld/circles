b69d758709d5a11bcb957c8cafeb85202d6d61e3
vincent.ftwprojects <vincent.ftwprojects@gmail.com>
2020-01-09 22:47:44 +0100

    draw 1024 circles on SFML. next step : - framerate / time measure - inport log into google sheet to see performance

--- Circles.cpp
+++ Circles.cpp
@@ -1,22 +1,57 @@
+//https://github.com/keldenworld/circles
 #include <iostream>
+#include <ctime>
 #include <SFML/Graphics.hpp>
+
+namespace ftw
+{
+    template <class T>
+    class container_basedonvector 
+    {
+        std::vector<T> datas;
+    public:
+        T* begin() { return (datas.size() == 0) ? nullptr : &datas.at(0); }
+        T* end() 
+        { 
+            T* lastelem = &datas.at(datas.size() - 1);
+            T* afterlastelem = ++lastelem;
+            return afterlastelem;
+        }
+
+        void add(const T& e) { datas.emplace_back(e); }
+        size_t size() { return datas.size(); }
+    };
+
+    template <class T>
+    using container = container_basedonvector<T>;
+}
 
 int main()
 {
-    std::cout << "Hello Kelden World with SFML!\n";
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!\n");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1024, 1024), "Hello Kelden World with SFML!");
+    auto circles = ftw::container<sf::CircleShape>();
+
+    sf::CircleShape shape(100);
+    for (int i = 0; i < 1024; i++)
+    {
+        shape.setPosition(static_cast<float>(rand() % 1024), static_cast<float>(rand() % 1024));
+        shape.setRadius(static_cast<float>(rand() % (255 - 5) + 5));
+        shape.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
+        circles.add(shape);
+    }
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
-        {
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
+
         window.clear();
-        window.draw(shape);
+
+        for (auto &s : circles)
+            window.draw(s);
+
         window.display();
     }
     return 0;
